7083cfb094f7326f03ccbedfa56010d741cd46ea
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-02-28 16:02:47 +0530

    automatic slider done

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react'
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import React, { useState,useRef } from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "./slider.scss"
+
 const Slider = () => {
-  const [currentSlide,setCurrentSlide]=useState(0)
-  const data=["/img/pexels-royal-anwar-983497 (1).jpg","/img/pexels-spencer-selover-428338.jpg","/img/pexels-spencer-selover-428340.jpg"]
-
-  function prevSlide(){
-    setCurrentSlide(currentSlide===0?2:(prev)=>prev-1)
-  }
-
-    function nextSlide() {
-      setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
-    }
-
-
+  const data = [
+    "/img/pexels-royal-anwar-983497 (1).jpg",
+    "/img/pexels-spencer-selover-428338.jpg",
+    "/img/pexels-spencer-selover-428340.jpg",
+    "/Horizontal/8.jpg",
+    "/Horizontal/9.jpg",
+    "/Horizontal/10.jpg",
+  ];
   return (
-    <div className="slider w-screen">
-      <div className="slider-images flex" style={{transform:`translateX(-${currentSlide*100}vw)`}} >
-      <img src={data[0]} alt="" />
-      <img src={data[1]} alt="" />
-      <img src={data[2]} alt="" />
-      </div>
-
-      <div className="slider-buttons-container">
-        <div className="left-icon icon" onClick={prevSlide}>
-          <ArrowBackIcon></ArrowBackIcon>
-        </div>
-        <div className="right-icon icon" onClick={nextSlide}>
-          <ArrowForwardIcon></ArrowForwardIcon>
-        </div>
-      </div>
+    <div className="automaticSlider h-svh w-[75%] mx-auto object-cover rounded-md mt-8 overflow-hidden">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {data.map(ele=><SwiperSlide><img src={ele} className='object-cover h-[px] '></img></SwiperSlide>)}
+      </Swiper>
     </div>
   );
 }
