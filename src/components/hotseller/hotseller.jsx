971effd5383962e0c30bd8f5df19c5dba85893cc
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-12-30 17:49:18 +0530

    ui imporved and cart page added

--- src/components/hotseller/hotseller.jsx
+++ src/components/hotseller/hotseller.jsx
@@ -4,7 +4,7 @@ const Hotseller = () => {
   return (
     <div className='text-center flex flex-col gap-8 max-w-screen-xl mx-auto mt-8 md:px-8 px-4'>
     <h2 className='font-semibold text-2xl'>HOTSELLER</h2>
-    <div className='grid md:grid-cols-4 grid-cols-2 gap-8'>
+    <div className='grid md:grid-cols-4 grid-cols-2 sm:gap-8 gap-2'>
       <div><img src='/Horizontal/7.jpg'></img></div>
       <div><img src='/Horizontal/9.jpg'></img></div>
       <div><img src='/Horizontal/2.jpg'></img></div>
