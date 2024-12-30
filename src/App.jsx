c9f0aaab9cab14eb7e7196456eb76f5a20cef467
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-12-30 16:14:29 +0530

    homepage layput created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import "./index.css";
-import Product from "./pages/Product/Product";
-import Products from "./pages/Products/Products";
+// import Product from "./pages/Product/Product";
+// import Products from "./pages/Products/Products";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import ArtistProducts from "./components/artists/ArtistProducts";
+// import ArtistProducts from "./components/artists/ArtistProducts";
 import TopHeader from "./components/topHeader/topHeader";
-import Home from "./pages/home/Home";
+// import Home from "./pages/home/Home";
 
 const Layout = () => {
   return (
@@ -25,22 +25,22 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout></Layout>,
     children: [
-      {
-        path: "/products/:id",
-        element: <Products></Products>,
-      },
-      {
-        path: "/artists/:name",
-        element: <ArtistProducts></ArtistProducts>,
-      },
-      {
-        path: "/product/:id",
-        element: <Product></Product>,
-      },
-      {
-        path: "/",
-        element: <Home />,
-      },
+      // {
+      //   path: "/products/:id",
+      //   // element: <Products></Products>,
+      // },
+      // {
+      //   path: "/artists/:name",
+      //   element: <ArtistProducts></ArtistProducts>,
+      // },
+      // {
+      //   path: "/product/:id",
+      //   element: <Product></Product>,
+      // },
+      // {
+      //   path: "/",
+      //   element: <Home />,
+      // },
     ],
   },
 ]);
